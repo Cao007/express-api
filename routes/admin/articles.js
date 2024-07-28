6100fcc4f892dd2aedd308fddfbe29fa1c9f3155
Cao007 <1114685490@qq.com>
2024-07-28 20:44:22 +0800

    封装utils/response.js

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { Article } = require('../../models');
 const { Op } = require('sequelize');
+const {
+    NotFoundError,
+    success,
+    failure
+} = require('../../utils/response');
 
 /**
  * 查询文章列表 
@@ -12,19 +17,18 @@ router.get('/', async function (req, res, next) {
         // 获取查询参数（query参数）
         let { currentPage, pageSize, title } = req.query;
 
-        // 如果没有传递这两个参数，就使用默认值
+        // 分页查询
         // /admin/articles?currentPage=1&pageSize=10
         currentPage = Math.abs(Number(currentPage)) || 1;
         pageSize = Math.abs(Number(pageSize)) || 10;
         const offset = (currentPage - 1) * pageSize;
-
         const conditions = {
             order: [['id', 'DESC']],
             limit: pageSize,
             offset: offset,
         };
 
-        // 如果有查询参数title，则添加模糊查询条件
+        // 模糊查询
         // /admin/articles?title=xxx
         if (title) {
             conditions.where = {
@@ -34,12 +38,12 @@ router.get('/', async function (req, res, next) {
             }
         };
 
+        // 操作数据库：查找所有文章
         const { count, rows } = await Article.findAndCountAll(conditions);
 
-        res.json({
-            status: true,
-            message: '查询文章列表成功',
-            data: rows,
+        // 成功响应
+        success(res, '查询文章列表成功', {
+            articles: rows,
             pagination: {
                 total: count,
                 currentPage,
@@ -47,22 +51,8 @@ router.get('/', async function (req, res, next) {
             }
         })
     } catch (error) {
-        if (error.name === 'SequelizeValidationError') {
-            const errors = error.errors.map(err => err.message);
-            res.status(400).json({
-                status: false,
-                message: '参数验证失败',
-                errors
-            })
-        } else {
-            res.status(500).json({
-                status: false,
-                message: '查询文章列表失败',
-                data: {
-                    errors: [error.message]
-                }
-            })
-        }
+        // 错误响应
+        failure(res, error);
     }
 });
 
@@ -73,39 +63,12 @@ router.get('/', async function (req, res, next) {
  */
 router.get('/:id', async function (req, res, next) {
     try {
-        const { id } = req.params;
+        // 查询当前文章
+        const article = await getArticleByID(req)
 
-        const article = await Article.findByPk(id);
-
-        if (article) {
-            return res.json({
-                status: true,
-                message: '查询文章详情成功',
-                data: article
-            })
-        } else {
-            return res.status(404).json({
-                status: false,
-                message: '文章不存在',
-            })
-        }
+        success(res, `查询文章详情成功`, { article })
     } catch (error) {
-        if (error.name === 'SequelizeValidationError') {
-            const errors = error.errors.map(err => err.message);
-            res.status(400).json({
-                status: false,
-                message: '参数验证失败',
-                errors
-            })
-        } else {
-            res.status(500).json({
-                status: false,
-                message: '查询文章详情失败',
-                data: {
-                    errors: [error.message]
-                }
-            })
-        }
+        failure(res, error);
     }
 })
 
@@ -116,37 +79,15 @@ router.get('/:id', async function (req, res, next) {
  */
 router.post('/', async function (req, res, next) {
     try {
-        const { title, content } = req.body;
+        // 获取body参数
+        const body = filterBody(req);
 
-        const article = await Article.create({
-            title,
-            content
-        });
+        // 操作数据库： 创建文章
+        const article = await Article.create(body);
 
-        // 201表示新建了资源
-        res.status(201).json({
-            status: true,
-            message: '创建文章成功',
-            data: article
-        })
-
+        success(res, '创建文章成功', { article }, 201); // 201表示新建了资源
     } catch (error) {
-        if (error.name === 'SequelizeValidationError') {
-            const errors = error.errors.map(err => err.message);
-            res.status(400).json({
-                status: false,
-                message: '参数验证失败',
-                errors
-            })
-        } else {
-            res.status(500).json({
-                status: false,
-                message: '创建文章失败',
-                data: {
-                    errors: [error.message]
-                }
-            })
-        }
+        failure(res, error);
     }
 })
 
@@ -157,40 +98,15 @@ router.post('/', async function (req, res, next) {
  */
 router.delete('/:id', async function (req, res, next) {
     try {
-        const { id } = req.params;
+        // 查询当前文章
+        const article = await getArticleByID(req)
 
-        const article = await Article.findByPk(id);
+        // 操作数据库：删除文章
+        await article.destroy();
 
-        if (article) {
-            await article.destroy();
-
-            res.json({
-                status: true,
-                message: '删除文章成功',
-            })
-
-        } else
-            res.status(404).json({
-                status: false,
-                message: '文章不存在',
-            })
+        success(res, '删除文章成功')
     } catch (error) {
-        if (error.name === 'SequelizeValidationError') {
-            const errors = error.errors.map(err => err.message);
-            res.status(400).json({
-                status: false,
-                message: '参数验证失败',
-                errors
-            })
-        } else {
-            res.status(500).json({
-                status: false,
-                message: '删除文章失败',
-                data: {
-                    errors: [error.message]
-                }
-            })
-        }
+        failure(res, error);
     }
 })
 
@@ -201,46 +117,50 @@ router.delete('/:id', async function (req, res, next) {
  */
 router.put('/:id', async function (req, res, next) {
     try {
-        const { id } = req.params;
-        const { title, content } = req.body;
+        // 查询当前文章
+        const article = await getArticleByID(req);
 
-        const article = await Article.findByPk(id);
+        // 获取body参数
+        const body = filterBody(req);
 
-        if (article) {
-            await article.update({
-                title,
-                content
-            });
+        // 操作数据库： 更新文章
+        await article.update(body);
 
-            res.json({
-                status: true,
-                message: '更新文章成功',
-                data: article
-            })
-        } else
-            res.status(404).json({
-                status: false,
-                message: '文章不存在',
-            })
-
+        success(res, '更新文章成功', { article });
     } catch (error) {
-        if (error.name === 'SequelizeValidationError') {
-            const errors = error.errors.map(err => err.message);
-            res.status(400).json({
-                status: false,
-                message: '参数验证失败',
-                errors
-            })
-        } else {
-            res.status(500).json({
-                status: false,
-                message: '更新文章失败',
-                data: {
-                    errors: [error.message]
-                }
-            })
-        }
+        failure(res, error);
     }
 })
+
+/**
+ * 公共方法：查询当前文章
+ */
+async function getArticleByID(req) {
+    // 获取params参数（path参数）
+    const { id } = req.params;
+
+    // 操作数据库： 查询当前文章通过id
+    const article = await Article.findByPk(id);
+
+    if (!article) {
+        throw new NotFoundError(`ID: ${id}的文章未找到。`)
+    }
+
+    // 返回响应数据
+    return article;
+}
+
+/**
+ * 公共方法：白名单过滤
+ * @param req
+ * @returns {{title, content: (string|string|DocumentFragment|*)}}
+ */
+function filterBody(req) {
+    // 返回白名单过滤后的body参数，忽略用户上传的非法字段
+    return {
+        title: req.body.title,
+        content: req.body.content
+    };
+}
 
 module.exports = router;
