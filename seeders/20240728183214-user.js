6b8aeb943525c4e6f7b7308703ee63c2abcd1b56
Cao007 <1114685490@qq.com>
2024-07-29 12:34:59 +0800

    bcryptjs加密数据

--- seeders/20240728183214-user.js
+++ seeders/20240728183214-user.js
@@ -1,13 +1,14 @@
 'use strict';
+const bcrypt = require('bcryptjs')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [
       {
-        email: 'admin@clwy.cn',
+        email: 'admin@qq.cn',
         username: 'admin',
-        password: '123123',
+        password: bcrypt.hashSync('123123', 10),
         nickname: '超厉害的管理员',
         sex: 2,
         role: 100,
@@ -15,9 +16,9 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        email: 'user1@clwy.cn',
+        email: 'user1@qq.cn',
         username: 'user1',
-        password: '123123',
+        password: bcrypt.hashSync('123123', 10),
         nickname: '普通用户1',
         sex: 0,
         role: 0,
@@ -25,9 +26,9 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        email: 'user2@clwy.cn',
+        email: 'user2@qq.cn',
         username: 'user2',
-        password: '123123',
+        password: bcrypt.hashSync('123123', 10),
         nickname: '普通用户2',
         sex: 0,
         role: 0,
@@ -35,9 +36,9 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        email: 'user3@clwy.cn',
+        email: 'user3@qq.cn',
         username: 'user3',
-        password: '123123',
+        password: bcrypt.hashSync('123123', 10),
         nickname: '普通用户3',
         sex: 1,
         role: 0,
