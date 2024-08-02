83d91074b55e64c080c3a1c2b262d92192ad04d6
Cao007 <1114685490@qq.com>
2024-08-02 16:45:04 +0800

    前台首页、分类、课程接口

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const logger = require('morgan');
 require('dotenv').config(); // 导入环境变量
 const adminAuth = require('./middlewares/admin-auth');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 // 导入后台路由文件
 const adminArticlesRouter = require('./routes/admin/articles');
 const adminCategoriesRouter = require('./routes/admin/categories');
@@ -16,17 +14,20 @@ const adminCoursesRouter = require('./routes/admin/courses');
 const adminChaptersRouter = require('./routes/admin/chapters');
 const adminChartsRouter = require('./routes/admin/charts');
 const adminAuthRouter = require('./routes/admin/auth');
+// 导入前台路由文件
+const frontHomeRouter = require('./routes/front/home');
+const frontCategoriesRouter = require('./routes/front/categories');
+const frontCoursesRouter = require('./routes/front/courses');
 
 const app = express();
-
+// 常用中间件
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 // 后台路由配置
 app.use('/admin/articles', adminAuth, adminArticlesRouter);
 app.use('/admin/categories', adminAuth, adminCategoriesRouter);
@@ -36,5 +37,9 @@ app.use('/admin/courses', adminAuth, adminCoursesRouter);
 app.use('/admin/chapters', adminAuth, adminChaptersRouter);
 app.use('/admin/charts', adminAuth, adminChartsRouter);
 app.use('/admin/auth', adminAuthRouter);
+// 前台路由配置
+app.use('/front/home', frontHomeRouter);
+app.use('/front/categories', frontCategoriesRouter);
+app.use('/front/courses', frontCoursesRouter);
 
 module.exports = app;
