3826b4af35e7cd1ae2e3808109471d38a3617f68
Cao007 <1114685490@qq.com>
2025-01-05 22:00:18 +0800

    创建模型、迁移文件

---
+++ migrations/20250105114303-create-category.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      rank: {
+        allowNull: false,
+        defaultValue: 1,
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Categories');
+  }
+};
