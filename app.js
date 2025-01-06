95855613a64b1b83683b88a5c741b355c82f103a
Cao007 <1114685490@qq.com>
2025-01-06 23:22:06 +0800

    echarts接口

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const adminSettingsRouter = require('./routes/admin/settings');
 const adminUsersRouter = require('./routes/admin/users');
 const adminCoursesRouter = require('./routes/admin/courses');
 const adminChaptersRouter = require('./routes/admin/chapters');
+const adminChartsRouter = require('./routes/admin/charts');
 
 const app = express();
 
@@ -28,5 +29,6 @@ app.use('/admin/settings', adminSettingsRouter);
 app.use('/admin/users', adminUsersRouter);
 app.use('/admin/courses', adminCoursesRouter);
 app.use('/admin/chapters', adminChaptersRouter);
+app.use('/admin/charts', adminChartsRouter);
 
 module.exports = app;
