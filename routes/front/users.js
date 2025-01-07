7683595074dae4dc8d82c1e2f4993d3030f5732d
Cao007 <1114685490@qq.com>
2025-01-07 16:22:43 +0800

    更新账户信息

--- routes/front/users.js
+++ routes/front/users.js
@@ -41,6 +41,47 @@ router.put('/info', async function (req, res) {
 });
 
 /**
+ * 更新账户信息
+ * PUT /users/account
+ */
+router.put('/account', async function (req, res) {
+  try {
+    const body = {
+      email: req.body.email,
+      username: req.body.username,
+      currentPassword: req.body.currentPassword,
+      password: req.body.password,
+      passwordConfirmation: req.body.passwordConfirmation
+    };
+
+    if (!body.currentPassword) {
+      throw new BadRequestError('当前密码必须填写。');
+    }
+
+    if (body.password !== body.passwordConfirmation) {
+      throw new BadRequestError('两次输入的密码不一致。');
+    }
+
+    // 加上 true 参数，可以查询到加密后的密码
+    const user = await getUser(req, true);
+
+    // 验证当前密码是否正确
+    const isPasswordValid = bcrypt.compareSync(body.currentPassword, user.password);
+    if (!isPasswordValid) {
+      throw new BadRequestError('当前密码不正确。');
+    }
+
+    await user.update(body);
+
+    // 删除密码字段
+    delete user.dataValues.password;
+    success(res, '更新账户信息成功。', { user });
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+/**
  * 公共方法：查询当前用户
  * @param req
  * @param showPassword
