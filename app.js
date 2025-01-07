eeb125871557cc4c4e47412605b96d99b1e976cb
Cao007 <1114685490@qq.com>
2025-01-07 17:29:38 +0800

    cors跨域配置

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 const adminAuth = require('./middlewares/admin-auth');
 const userAuth = require('./middlewares/user-auth');
 require('dotenv').config();
+const cors = require('cors')
 
 // 前台路由文件
 const indexRouter = require('./routes/front/home');
@@ -35,6 +36,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// CORS 跨域配置
+const corsOptions = {
+  origin: [
+    'https://clwy.cn',
+    'http://localhost:5500'
+  ],
+}
+app.use(cors(corsOptions));
 
 // 前台路由配置
 app.use('/front/home', indexRouter);
